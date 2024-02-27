098c3321c724e71d9ef869aa00c8bc46b885b8b0
rakeshk0 <rakeskumarkumar0@gmail.com>
2024-02-27 23:18:58 +0530

    react router

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+     <h1 className='bg-orange-400 text-white'>Currency Convertor</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 07reactrouter/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Footer from './Footer/Footer'
+import Header from './Header/Header'
+import Home from './Home/Home'
+
+function App() {
+  
+
+  return (
+    <>
+     <Header/>
+     <Home/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
